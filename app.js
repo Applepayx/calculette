000566eef10c06b04d5ae8f3a3c94abd380a88aa
deepshadow3rd <110748719+deepshadow3rd@users.noreply.github.com>
2025-01-07 22:13:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function checkPassword() {
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
-    if (password === '1122') { // Set your desired password here
+    if (password === 'aabb') { // Set your desired password here
         document.getElementById('password-screen').style.display = 'none';
         document.getElementById('gallery').style.display = 'block';
     } else if (password !== '') {
