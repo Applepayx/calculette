cde2197f81c64884fa8d3f863523db9817c02b73
deepshadow3rd <110748719+deepshadow3rd@users.noreply.github.com>
2025-01-08 06:02:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-function checkPassword(event) {
-    event.preventDefault(); // Prevent form submission
-
+function checkPassword() {
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
     
