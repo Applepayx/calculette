b12d4045784adb655f2718821818baddb44bf234
deepshadow3rd <110748719+deepshadow3rd@users.noreply.github.com>
2025-01-08 04:36:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function checkPassword() {
         document.getElementById('password-screen').style.display = 'none';
         document.getElementById('gallery').style.display = 'block';
     } else if (password === 'bbaa') { // If password is 'bbaa', load a different HTML page
-        window.location.href = 'd2386e91ff2e2d5b2b4f4d5d0c8a9e0f.html'; // Replace 'differentPage.html' with your desired page
+        window.location.href = 'insta.html'; // Replace 'differentPage.html' with your desired page
     } else if (password !== '') { // If password is incorrect
         errorMessage.textContent = 'Incorrect ID. Please try again.';
         errorMessage.style.color = 'red';
