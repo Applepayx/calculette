5a9857bbe020156f90aa997ecec63536a2b905f3
deepshadow3rd <110748719+deepshadow3rd@users.noreply.github.com>
2025-01-08 04:07:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 function checkPassword() {
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
-    
+    const iframeContainer = document.getElementById('iframe-container');
+    const galleryContainer = document.getElementById('gallery');
+
     if (password === 'aabb') { // If password is 'aabb', load the gallery
         document.getElementById('password-screen').style.display = 'none';
-        document.getElementById('gallery').style.display = 'block';
-    } else if (password === 'bbaa') { // If password is 'bbaa', load an online page
-        window.location.href = 'https://deepshadow3rd.github.io/Apple/'; // Replace with the online page URL
+        iframeContainer.style.display = 'none';
+        galleryContainer.style.display = 'block';
+    } else if (password === 'bbaa') { // If password is 'bbaa', load online page in iframe
+        document.getElementById('password-screen').style.display = 'none';
+        galleryContainer.style.display = 'none';
+        iframeContainer.style.display = 'block';
+        document.getElementById('iframe').src = 'https://deepshadow3rd.github.io/Apple/; // Replace with your online page URL
     } else if (password !== '') { // If password is incorrect
         errorMessage.textContent = 'Incorrect ID. Please try again.';
         errorMessage.style.color = 'red';
